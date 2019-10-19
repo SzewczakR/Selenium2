c535727835579aec53dba262ed7aa4bc483253e8
Bartosz Górny <johndoe@example.com>
2019-10-19 16:57:45 +0200

    Drugi test - wyszukaj Lublin

--- src/test/java/PierwszyTestWSelenium.java
+++ src/test/java/PierwszyTestWSelenium.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,19 +19,38 @@ public class PierwszyTestWSelenium {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tester\\Desktop\\jquerry\\Selenium\\chromedriver.exe");
         options = new ChromeOptions();
         options.addArguments("start-maximized");
-
+        driver = new ChromeDriver(options);
     }
 
+ //   @Test
+ //   public void otwarciePrzegladarki(){
+  //       driver.get("http://google.pl");
+   // }
     @Test
-    public void OtwarciePrzegladarki(){
-         driver = new ChromeDriver(options);
-
+    public void wyszukajLublin() {
+        driver.get("http://google.pl");
+        driver.findElement(By.cssSelector("[name=q]")).sendKeys("Lublin");
+        driver.findElement(By.cssSelector("[name=q]")).sendKeys(Keys.RETURN);
+        boolean nieZnaleziono = true;
+        while (nieZnaleziono){
+            if(driver.findElement(By.cssSelector("[href='https://pl.wikipedia.org/wiki/Lublin']:first-child")).isDisplayed()){
+                nieZnaleziono = false;
+            } else{
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        driver.findElement(By.cssSelector("[href='https://pl.wikipedia.org/wiki/Lublin']:first-child")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".firstHeading")).getText(),"Lublin[edytuj]");
     }
 
     @AfterMethod
     public void tearDown(){
 
-        driver.close();
+    //    driver.close();
     }
 
 }
